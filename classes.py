d6ef983777597634b178afc9e232f1a11935d6d9
Will <will115599c@gmail.com>
2024-10-29 14:48:16 -0700

    Added speed to each pokemon as well as priority to attacks such as Quick Attack.

--- classes.py
+++ classes.py
@@ -14,23 +14,26 @@ poke_type = {
 }
 
 moves = {
-    "Tackle": 10,
-    "Quick Attack": 12,
-    "Thunderbolt": 15,
-    "Ember": 14,
-    "Water Gun": 14
+    "Tackle": (10, 0, 0),               # (damage, if_elemental 0 == no  1 == yes, if_priority_attack 0 == no  1 == yes)
+    "Quick Attack": (12, 0, 1),
+    "Thunderbolt": (14, 1, 0),
+    "Ember": (14, 1, 0),
+    "Water Gun": (14, 1, 0)
 }
 
 class Pokemon:
-    def __init__(self, name, hp, moves, poke_type, poke_type_value):
+    def __init__(self, name, hp, moves, poke_type, poke_type_value, poke_speed):
         self.name = name
         self.hp = hp
         self.moves = moves
         self.poke_type = poke_type
         self.poke_type_value = poke_type_value
+        self.poke_speed = poke_speed
 
         self.moves_list = list(self.moves.items())
-        self.moves_list_damage = list(self.moves.values())
+        self.moves_list_damage = [move[0] for move in self.moves.values()]
+        self.moves_list_typing = [move[1] for move in self.moves.values()]
+        self.moves_list_priority = [move[2] for move in self.moves.values()]
 
     def get_type_value(self):
         return self.poke_type_value
@@ -38,80 +41,50 @@ class Pokemon:
     def get_name(self):
         return self.name
 
-    def take_damage(self, damage, attacker_type):
-        if damage == 14:
-            if self.poke_type_value == 1 and attacker_type == 1:
-                damage = damage
-                self.hp -= damage
-                print("Normal Effective")
-            elif self.poke_type_value == 1 and attacker_type == 2:
-                damage = damage / 2
-                self.hp -= damage
-                print("Not Very Effective")
-            elif self.poke_type_value == 1 and attacker_type == 3:
-                damage = damage / 2
-                self.hp -= damage
-                print("Not Very Effective")
-            elif self.poke_type_value == 1 and attacker_type == 4:
-                damage = damage * 1.5
-                self.hp -= damage
-                print("Super Effective!")
+    def take_damage(self, damage, damage_type, pokemon_name_attacker, move_name, attacker_type):
+        effectiveness = {
+            (1, 1): 1,
+            (1, 2): 0.5,
+            (1, 3): 0.5,
+            (1, 4): 1.5,
 
-            if self.poke_type_value == 2 and attacker_type == 1:
-                damage = damage * 1.5
-                self.hp -= damage
-                print("Super Effective!")
-            elif self.poke_type_value == 2 and attacker_type == 2:
-                damage = damage
-                self.hp -= damage
-                print("Normal Effective")
-            elif self.poke_type_value == 2 and attacker_type == 3:
-                damage = damage * 1.5
-                self.hp -= damage
-                print("Super Effective!")
-            elif self.poke_type_value == 2 and attacker_type == 4:
-                damage = damage / 2
-                self.hp -= damage
-                print("Not Very Effective")
+            (2, 1): 1.5,
+            (2, 2): 1,
+            (2, 3): 1.5,
+            (2, 4): 0.5,
 
-            if self.poke_type_value == 3 and attacker_type == 1:
-                damage = damage * 1.5
-                self.hp -= damage
-                print("Super Effective!")
-            elif self.poke_type_value == 3 and attacker_type == 2:
-                damage = damage / 2
-                self.hp -= damage
-                print("Not Very Effective")
-            elif self.poke_type_value == 3 and attacker_type == 3:
-                damage = damage
-                self.hp -= damage
-                print("Normal Effective")
-            elif self.poke_type_value == 3 and attacker_type == 4:
-                damage = damage * 1.5
-                self.hp -= damage
-                print("Super Effective!")
+            (3, 1): 1.5,
+            (3, 2): 0.5,
+            (3, 3): 1,
+            (3, 4): 1.5,
 
-            if self.poke_type_value == 4 and attacker_type == 1:
-                damage = damage / 2
-                self.hp -= damage
-                print("Not Very Effective")
-            elif self.poke_type_value == 4 and attacker_type == 2:
-                damage = damage * 1.5
-                self.hp -= damage
-                print("Super Effective!")
-            elif self.poke_type_value == 4 and attacker_type == 3:
-                damage = damage / 2
-                self.hp -= damage
-                print("Not Very Effective")
-            elif self.poke_type_value == 4 and attacker_type == 4:
-                damage = damage
-                self.hp -= damage
-                print("Normal Effective")
+            (4, 1): 0.5,
+            (4, 2): 1.5,
+            (4, 3): 0.5,
+            (4, 4): 1,
+        }
+
+        multiplier = effectiveness.get((self.poke_type_value, attacker_type))
+
+        if damage_type == 1:
+            adjusted_damage = damage * multiplier
         else:
-            print("Normal Effective")
-            self.hp -= damage
+            multiplier = 1
+            adjusted_damage = damage
 
-        print(f"Hp reduced to {self.hp}\n")
+        self.hp -= adjusted_damage
+
+        print(f"{pokemon_name_attacker} uses {move_name} on {self.name}")
+
+
+        if multiplier == 1:
+            print(f"Normal Effective damage on {self.name}!")
+        elif multiplier == 0.5:
+            print(f"Not Very Effective damage on {self.name}!")
+        elif multiplier == 1.5:
+            print(f"Very Effective damage on {self.name}!")
+
+        print(f"{self.name} Hp reduced to {self.hp}\n")
 
     def heal(self):
         self.hp = self.hp + 20
@@ -119,16 +92,24 @@ class Pokemon:
     def is_fainted(self):
         return self.hp <= 0
 
+    def get_speed(self):
+        return self.poke_speed
+
     def choose_move(self):
-        for index, (move, damage) in enumerate(self.moves_list, start=1):
+        for index, (move, (damage, _, _)) in enumerate(self.moves_list, start=1):
             print(f"{index}, {move} - Damage: {damage} ")
         print("Input 1-3 to choose a move")
 
         choice = int(input("Enter your choice: ")) - 1
 
         damage = self.moves_list_damage[choice]
+        type_value = self.moves_list_typing[choice]
+        pokemon = self.name
+        move_name = self.moves_list[choice][0]
+        if_priority = self.moves_list_priority[choice]
+        print("\n")
 
-        return damage;
+        return damage, type_value, pokemon, move_name, if_priority
 
 
 
--- functions.py
+++ functions.py
@@ -1,18 +1,50 @@
 from classes import *
+import random
 
 
-charmander = Pokemon("Charmander", 100, {"Tackle" : 10, "Quick Attack": 12, "Ember": 14}, "Water", 2)
-piplup = Pokemon("Piplup", 100, {"Tackle" : 10, "Quick Attack": 12, "Water Gun": 14}, "Fire", 1)
+charmander = Pokemon("Charmander", 100, {"Tackle": (10, 0, 0), "Quick Attack": (12, 0, 1), "Ember": (14, 1, 0)}, "Water", 2, 10)
+piplup = Pokemon("Piplup", 100, {"Tackle": (10, 0, 0), "Quick Attack": (12, 0, 1), "Water Gun": (14, 1, 0)}, "Fire", 1, 11)
 
 def battle(pokemon1, pokemon2):
     print(f"Battle Start! {pokemon1.get_name()} vs {pokemon2.get_name()}")
 
     while not pokemon1.is_fainted() and not pokemon2.is_fainted():
-        print(f"{pokemon1.get_name()}'s turn.")
-        pokemon2.take_damage(pokemon1.choose_move(), pokemon1.get_type_value())
+        print(f"{pokemon1.get_name()}'s choice of move.")
+        damage1, move_type1, pokemon_1, move_attacker_1, is_priority_1 = pokemon1.choose_move()
 
-        print(f"{pokemon2.get_name()}'s turn.")
-        pokemon1.take_damage(pokemon2.choose_move(), pokemon2.get_type_value())
+        print(f"{pokemon2.get_name()}'s choice of move.")
+        damage2, move_type2, pokemon_2, move_attacker_2, is_priority_2 = pokemon2.choose_move()
+
+        if is_priority_1 == 0 and is_priority_2 == 0:
+            if pokemon1.get_speed() > pokemon2.get_speed():
+                pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+                pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+            elif pokemon1.get_speed() < pokemon2.get_speed():
+                pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+                pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+            else:
+                result = random.choice([1, 2])
+                if result == 1:
+                    pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+                    pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+                else:
+                    pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+                    pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+        else:
+            if is_priority_1 == 1 and is_priority_2 == 0 :
+                pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+                pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+            elif is_priority_1 == 0 and is_priority_2 == 1 :
+                pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+                pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+            else:
+                result = random.choice([1, 2])
+                if result == 1:
+                    pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
+                    pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+                else:
+                    pokemon1.take_damage(damage2, move_type2, pokemon_2, move_attacker_2, pokemon2.get_type_value())
+                    pokemon2.take_damage(damage1, move_type1, pokemon_1, move_attacker_1, pokemon1.get_type_value())
 
     if pokemon1.is_fainted():
         print(f"{pokemon1.get_name()} fainted.")
